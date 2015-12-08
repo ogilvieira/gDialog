60856c621cd8fd5f58a1aebaf75a56e9c45d3a56
Gil Vieira <ogilvieira@gmail.com>
2015-12-08 14:24:17 -0200

    add modulos

--- dist/jquery.gDialog.js
+++ dist/jquery.gDialog.js
@@ -39,27 +39,120 @@
 }(function($){
     var m = {};
  	var g = {};
-
+    m.OPENING = false;
     m.OPTIONS = {
         title: false,
         animateIn : false,
         animateOut : false,
+        onSubmit : false,
+        onCancel : false,
+    };
+
+    m.tplBase = "<div class=\"gdialog-wrap\">";
+    m.tplBase += "<div class=\"gdialog-container\">";
+    m.tplBase += "{{HEADER}}";
+    m.tplBase += "<div class=\"gdialog-content\">{{message}}{{INPUT}}</div>";
+    m.tplBase += "<div class=\"gdialog-button-group\">{{BUTTON_CANCEL}} <button class=\"button button-ok\">Ok</button></div>";
+    m.tplBase += "</div>";
+    m.tplBase += "</div>";
+
+    m.tplHeader = "<div class=\"gdialog-header\">{{title}}</div>";
+    m.tplInput = "<div class=\"gdialog-field\"><input type=\"text\"></div>";
+
+    m.getTeplate = function(type, message, options){
+        var t = m.tplBase;
+        
+        if( type !== 'alert' ){ 
+            t = t.replace("{{BUTTON_CANCEL}}", "<button class=\"button button-cancel\">Cancel</button>"); 
+        } else {
+            t = t.replace("{{BUTTON_CANCEL}}", ""); 
+        }
+        if( type == 'prompt' ){ 
+            t = t.replace("{{INPUT}}", m.tplInput); 
+        } else {
+            t = t.replace("{{INPUT}}", ""); 
+        }        
+        if( options.title ){ 
+            t = t.replace("{{HEADER}}", m.tplHeader.replace("{{title}}", options.title) ); 
+        } else {
+            t = t.replace("{{HEADER}}", ""); 
+        }
+        t = t.replace("{{message}}", message ); 
+        return t;
     };
 
     m.clear = function(){
         $('.gdialog-shadow').length ? $('.gdialog-shadow').remove() : '';
+        $('.gdialog-wrap').length ? $('.gdialog-wrap').remove() : '';
     };
 
-    m.init = function(){
-        m.clear();
-        $('body').append("<div class=\"gdialog-shadow\"></div>");
+    m.Dialog = function(){
+        var that = this;
+        
+        that.close = function(){
+            $('.gdialog-shadow').addClass("animated fadeOut");
+            if( that.options.animateOut ){
+                if( that.options.animateIn ){ that.container.find('.gdialog-container').removeClass(that.options.animateIn) }
+                that.container.find('.gdialog-container').addClass('animated '+that.options.animateOut);
+                setTimeout(function(){
+                    that.container.removeClass('is-active');
+                    that.container.remove();
+                    m.OPENING = false;
+                    $('.gdialog-shadow').remove();
+                }, 800);
+            }else {
+                that.container.remove();
+                m.OPENING = false;
+            }
+        };
+
+        that.addEvents = function(){
+            that.btnOk.on("click", function(e){
+                e.preventDefault();
+                var res = false;
+                if( that.field.length ){ res = that.field.val(); }
+                if( typeof that.options.onSubmit == 'function' ){
+                    that.options.onSubmit(res);
+                }
+                that.close();
+            });
+            that.btnCancel.on("click", function(e){
+                e.preventDefault();
+                var res = false;
+                if( that.field.length ){ res = that.field.val(); }
+                if( typeof that.options.onCancel == 'function' ){
+                    that.options.onCancel(res);
+                }
+                that.close();
+            });
+        }
+
+        this.init = function(type, message, options, defaultValue){
+            if( m.OPENING ){ $('.gdialog-shadow, .gdialog-wrap').remove(); }
+            m.clear();
+
+            that.options = m.getOptions(options);
+            $('body').append("<div class=\"gdialog-shadow\"></div> "+m.getTeplate(type, message, that.options) );
+            that.container = $('body').find('.gdialog-wrap');
+            that.btnOk = that.container.find('.button-ok');
+            that.btnCancel = that.container.find('.button-cancel');
+            that.field = that.container.find('input');
+
+            that.container.addClass('is-active').css({'top': $(window).scrollTop()+50});
+            if( that.options.animateIn ){
+                that.container.find('.gdialog-container').addClass('animated '+that.options.animateIn);
+            }
+            m.OPENING = true;
+
+            that.addEvents();
+        };
     };
 
     m.getOptions = function(options){
         var o = m.OPTIONS;
         if( typeof options == 'object' ){
           $.each(options, function(key, val){
-            m.OPTIONS[key] !== undefined ? m.OPTIONS[key] = val : '';
+            o[key] !== undefined ? o[key] = val : console.error("The option \""+key+"\" not exist.");
           });
         };
         return o;
@@ -67,20 +160,30 @@
 
     //global functions
     g.alert = function(message, userOptions){
-        m.init();
-        var options = m.getOptions(userOptions);
+        var message = message || "";
+        var userOptions = userOptions || {};
+        var dialog = new m.Dialog;
+        dialog.init('alert', message, userOptions);
     };
 
     g.confirm = function(message, userOptions){
-        m.init();
-        var options = m.getOptions(userOptions);
+        var message = message || "";
+        var userOptions = userOptions || {};
+        m.Dialog('confirm', message, userOptions);  
     };
 
-	g.prompt = function(message, userOptions){
-        m.init();
-        var options = m.getOptions(userOptions);
-	};
+	g.prompt = function(message, defaultValue, userOptions){
+        var message = message || "";
+        var userOptions = userOptions || {};
+        m.Dialog('prompt', message, userOptions, defaultValue);
+    };
 
+    g.config = function(options){
+        if( typeof options !== 'object' ){ return false; }
+        $.each(options, function(key, val){
+            m.OPTIONS[key] !== undefined ? m.OPTIONS[key] = val : console.error("The option \""+key+"\" not exist.");
+        });
+    };
 
     $.gDialog = $.gDialog || g;
 }));
